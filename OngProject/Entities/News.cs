b7f64d3b2b25c311347397aefce4e174c9de8b0b
Enguelbert Garcia Pernalete <enguelbertg13@gmail.com>
2021-11-28 12:53:53 -0300

    Commit add news entity

---
+++ OngProject/Entities/News.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace OngProject.Entities
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Image { get; set; }
+
+        public DateTimeOffset Date { get; set; } = DateTime.Now; 
+
+        public bool IsDeleted { get; set; } = false;
+    }
+}
