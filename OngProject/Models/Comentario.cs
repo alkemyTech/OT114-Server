b7122227791e3674ea734d0d493635d31bec735c
churi <santiagom@gmail.com>
2021-12-08 09:52:35 -0300

    CreacionComentariosModelo

---
+++ OngProject/Models/Comentario.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Models
+{
+    public class Comentario
+    {
+        public int Id { get; set; }
+
+        /// Consultar al mentor tipo de dato ///
+        public string Body { get; set; }
+        
+        public User UserId { get; set; }
+
+        // TODO : Relacionar modelo ///
+        //public News NewsId { get; set; }
+    }
+}
