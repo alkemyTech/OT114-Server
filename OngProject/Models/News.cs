89791d3edb4556b588e2feff89cb3f61db9a803e
churi <santiagom@gmail.com>
2021-12-08 09:35:38 -0300

    Se creo News modelo

---
+++ OngProject/Models/News.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public string Text { get; set; }
+
+        [Required]
+        public byte[] Image { get; set; }
+
+        public Category Category { get; set; }
+       
+        //TODO : TimeStamp o SoftDelete //
+    }
+}
