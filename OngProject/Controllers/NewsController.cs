15a2f09ac9e82f27623002e5cd694c42f8771c68
lucianomascina <lucianomascina@gmail.com>
2021-12-08 15:16:54 -0300

    get news by id created

---
+++ OngProject/Controllers/NewsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OngProject.Controllers
+{
+    [Route("api/news")]
+    [ApiController]
+    public class NewsController : ControllerBase
+    {
+        private readonly INewsService _newsService;
+
+        public NewsController(INewsService newsService)
+        {
+            _newsService = newsService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<News>> GetById(int id)
+        {
+            var news = await _newsService.GetById();
+            
+            if (news == null)
+            {
+                return NotFound($"la news con id {id} no existe.");
+            }
+
+            return Ok(news);
+        }
+
+   
+    }
+}
