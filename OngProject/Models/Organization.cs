da0c6bc05fe3d50bdb17a32ad46c9ec6fa6df1e7
Alvaro D <alvarodeguer@gmail.com>
2021-12-26 23:45:22 -0300

    Update Organization

--- OngProject/Models/Organization.cs
+++ OngProject/Models/Organization.cs
@@ -8,15 +8,19 @@ namespace OngProject.Models
 {
     public class Organization
     {
+        [Required]
         public int Id { get; set; }
 
         [Required]
+        [StringLength(20)]
         public string Name { get; set; }
 
         public int Image { get; set; }
+
+        [StringLength(20)]
         [Required]
         public string Address { get; set; }
-        [Required]
+        
         public int Phone { get; set; }
 
         public DateTime? deletedAt { get; set; } = DateTime.Now;
